95a3b95b7a33fb3c04a29791ff035f41e4892f68
guidoftp <147518664+guidoftp@users.noreply.github.com>
2024-09-13 11:50:39 -0300

    app

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def app(request):
+    
+    capitan= {'pelicula': 'ciencia ficcion',
+              'autor' : 'Jack Kitby',
+              'Anio Salida' : '2026'}
+    
+    kunfupanda = {'pelicula' : 'entretenimiento',
+                  'autor' : 'DreamWorks',
+                  'Anio salida' : '2024'}
+    
+    soul = {'pelicula' : 'entretenimiento',
+            'autor' : 'Peter Docter',
+            'Anio salida' : '2024'}
+    
+    return app
